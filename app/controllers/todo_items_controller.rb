be3a4fc81ee69d343a697f88c254c1fd060b952c
rohit_mathew <rohit_mathew@apple.com>
2017-12-10 14:27:04 -0800

    Completed basic functionality

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
 
   def create
     @todo_item = @todo_list.todo_items.create(todo_items_params)
@@ -7,19 +8,27 @@ class TodoItemsController < ApplicationController
   end
 
   def destroy
-    @todo_item = @todo_list.todo_items.find(params[:id]) 
     if @todo_item.destroy
       flash[:success] = "Todo Item was deleted!"
     else
       flash[:error] = "Todo Item could not be deleted"
     end
-    redirect_to @todo_list
+    redirect_to @todo_list, notice: "Todo item completed"
+  end
+
+  def complete 
+      @todo_item.update_attribute(:completed_at, Time.now)
+      redirect_to @todo_list
   end
 
   private
 
   def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
   end
 
   def todo_items_params
